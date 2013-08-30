107038595f7b1367701c1b9afcaf874621b88ac0
Brent Nikolaus <brent.nikolaus@puppetlabs.com>
2013-08-29 23:05:37 -0700

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,12 @@ apt-get install git
 # 
 hostname master
 
-apt-get install puppetmaster # if debian
+
+apt-get install -y puppet puppet-common puppet-el puppet-testsuite \
+	puppetmaster puppetmaster-common vim-puppet
+
 # yum install puppet-server
+
+apt-get install -y puppetmaster-passenger
 
 
